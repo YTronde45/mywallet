d622b30bdecd458781567a7537ec5b2720619bf6
YTronde45 <tradeava729@gmail.com>
2025-05-04 10:59:37 -0700

    Create delegtare.sol

---
+++ delegtare.sol
@@ -0,0 +1,7 @@
+curl -X POST  http://127.0.0.1:8092/wallet/delegateresource -d  \
+'{"owner_address":"TR3e7bvoarT1dMtdnzyBVF8tJmfoorEU9J",
+ "receiver_address":"TFfJEwDFp5adstGi35MrtQiJ6f1FWgQPhd",
+ "balance":69000,
+ "resource":"BANDWIDTH",
+ "visible":true
+}'
